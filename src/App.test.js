73409308d932119ad96738f1b6f1fb50f6543ae2
Jose CM <jose.coego@gmail.com>
2025-01-05 10:52:14 +0100

    fix: Fix App tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders the App component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Task Timer/i);
+  const linkElement = screen.getByText(/Web pack Config/i);
   expect(linkElement).toBeInTheDocument();
 });
